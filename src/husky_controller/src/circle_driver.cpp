1209672bd97612a36748003a4c6fe52c7b3c47fe
akshaysheela95 <61129750+akshaysheela95@users.noreply.github.com>
2020-02-18 11:50:26 +0800

    Update circle_driver.cpp

--- src/husky_controller/src/circle_driver.cpp
+++ src/husky_controller/src/circle_driver.cpp
@@ -13,7 +13,7 @@ int main(int argc, char **argv) {
 
   while(ros::ok()) {
     geometry_msgs::Twist msg;
-    
+    msg.angular={0.0,0.0,0.5};
     pub.publish(msg);
     // TODO: Make Husky move in circles by publishing to husky_velocity_controller/cmd_vel topic.
 
