75893dd6858eea801a3baee1c3df455a809cbaa1
Andrew Sherry <andrew.sherry@chironix.com>
2022-02-19 13:52:48 +0800

    answers to circle driver and urdf fix

--- src/husky_controller/src/circle_driver.cpp
+++ src/husky_controller/src/circle_driver.cpp
@@ -1,19 +1,26 @@
 #include <ros/ros.h>
-// TODO: Include the appropriate header for sending Twist messages.
-#include  <stdlib.h>
+#include <geometry_msgs/Twist.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "husky_controller");
   ros::NodeHandle nh;
 
   // TODO: Create a publisher that publishes to husky_velocity_controller/cmd_vel topic.
+  ros::Publisher pub = nh.advertise<geometry_msgs::Twist>(
+    "husky_velocity_controller/cmd_vel", 100
+  );
 
   // Allows the publisher to publish at 10 Hz.
   ros::Rate rate(10);
 
+  geometry_msgs::Twist msg;
+  msg.linear.x = 0.75;
+  msg.angular.z = 0.5;
   while(ros::ok()) {
-    // TODO: Make Husky move in circles by publishing to husky_velocity_controller/cmd_vel topic.
-
+    pub.publish(msg);
     rate.sleep();
   }
+
+  return(0);
 }
